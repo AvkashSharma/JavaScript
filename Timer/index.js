2097329a9384dfb607a29c1234ffbb680c916752
Avkash <avkaashsharma89@gmail.com>
2020-03-14 16:29:33 -0400

    Functionality implemented, basic svg

--- Timer/index.js
+++ Timer/index.js
@@ -1,41 +1,15 @@
-class Timer {
-	constructor(durationInput, startButton, pauseButton) {
-		this.durationInput = durationInput;
-		this.startButton = startButton;
-		this.pauseButton = pauseButton;
-
-		this.startButton.addEventListener('click', this.start);
-	}
-	start() {
-		this.importantMethodToCall();
-	}
-
-	importantMethodToCall() {
-		console.log('IMPORTANT THING WAS DONE');
-	}
-
-	pause() {}
-	onDurationChange() {}
-	tick() {}
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
-// timer.start();
-
-// this is equal to whatever is to the left of the '.' in the method call
-const colors = {
-	printColor() {
-		console.log(this);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart() {
+		console.log('timer started');
+	},
+	onTick() {
+		console.log('timer ticked down');
+	},
+	onComplete() {
+		console.log('timer completed');
 	}
-};
-
-const randomObject = {
-	a: 1
-};
-
-randomObject.printColor = colors.printColor;
-randomObject.printColor();
+});
